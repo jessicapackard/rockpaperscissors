6b5569eb515b10b518289e0fb3de9d0bcdf0298f
Jessica Packard <jdpackard1@gmail.com>
2021-09-25 09:01:02 -0400

    playround as fx expr, moved inside game

--- scripts.js
+++ scripts.js
@@ -23,49 +23,49 @@ let playerPick = playerInput.toLowerCase();
 
 function game() {
 	
-	//let playerWins;
 	{let numberOfPlayerWins = 0;
 	let numberOfComputerWins = 0;
 	let score = 'The score is: \nYou - ' + numberOfPlayerWins + ', Computer - ' + numberOfComputerWins;
+
+	//function to play one round and declare winner of round
+let playRound = function(playerSelection, computerSelection) {
+	//to avoid "already declared" error in console, wrap in brackets to change scope
+	{let playerSelection = playerChoice();
+	let computerSelection = computerPlay();
+
+	if ((playerSelection === 'rock' && computerSelection === 'scissors')
+|| (playerSelection === 'paper' && computerSelection === 'rock') 
+|| (playerSelection === 'scissors' && computerSelection === 'paper')) {
+	console.log('You win this round! ' + playerSelection + ' beats ' + computerSelection);
+	return true;
+	}
+
+	else if (playerSelection === computerSelection) {
+	return "It's a tie. No winner this round.";}
+
+	else {
+	console.log('Dang. ' + computerSelection + ' beats ' + playerSelection + '. The computer wins this round.')
+	return false;
+	}
+}};
+
 for (let roundNumber = 1; roundNumber < 6; roundNumber++) {
 	console.log('ROUND '+ roundNumber);
-	
-	if (playRound() === true) {
+	console.log(playRound());
+	if (playRound === true) {
 		numberOfPlayerWins += 1;
 		console.log(score);
 	}
-	else {
+	else if (playRound === "It's a tie. No winner this round.") {
+		console.log(score);}
+	else if (playRound === false) {
 		numberOfComputerWins += 1;
 		console.log(score);}
-	
 }
 
 	//console.log('The score is: \nYou - ' + numberOfPlayerWins + ' Computer - ' + numberOfComputerWins);
 
 if (numberOfPlayerWins > numberOfComputerWins) {console.log('You won the game!!')}
 else if (numberOfPlayerWins < numberOfComputerWins) {console.log('You did not win the game :(')}}}
-
-
-//function to play one round and declare winner of round
-function playRound(playerSelection, computerSelection) {
-	//to avoid "already declared" error in console, wrap in brackets to change scope
-	{let playerSelection = playerChoice();
-	let computerSelection = computerPlay();
-	//let playerWins;
-if ((playerSelection === 'rock' && computerSelection === 'scissors')
-|| (playerSelection === 'paper' && computerSelection === 'rock') 
-|| (playerSelection === 'scissors' && computerSelection === 'paper')) {
-	console.log('You win this round!');
-	return true;
-	}
-
-else if (playerSelection === computerSelection) {
-	return "It's a tie. No winner this round.";}
-
-else {
-	console.log('Dang. The computer wins this round.')
-	return false;
-	}
-}}
 
 console.log(game());
