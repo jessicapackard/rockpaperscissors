7feab1eb11c193427b4da7eea50f9f33a913b680
Jessica Packard <jdpackard1@gmail.com>
2021-09-26 08:47:41 -0400

    polished up

--- scripts.js
+++ scripts.js
@@ -1,7 +1,8 @@
 //function to get random computer play from array and return in 'computerSelection' var
 
-function computerPlay() {
-	let computerChoice = possiblePlays[Math.floor(Math.random() * possiblePlays.length)];
+function getComputerPlay() {
+	let computerChoice = 
+		possiblePlays[Math.floor(Math.random() * possiblePlays.length)];
 	console.log('The computer chose ' + computerChoice);
 	return computerChoice;
 }
@@ -9,45 +10,52 @@ let possiblePlays = ['rock', 'paper', 'scissors'];
 
 //function to get playerInput and return case insensitive 'playerSelection'
 
-function playerChoice() {
+function getPlayerChoice() {
 let playerInput = prompt('Type one of three options: rock, paper, scissors', '');
 let playerPick = playerInput.toLowerCase();
-	if (playerPick !== 'rock' && playerPick !== 'paper' && playerPick !== 'scissors') {
-		alert('Invalid input. Refresh page and try again');	
+	if (playerPick !== 'rock' &&
+		playerPick !== 'paper' &&
+		playerPick !== 'scissors') {
+			alert('Invalid input. Refresh page and try again');	
 	}
 	else {
-	console.log('You chose '+ playerPick);
+		console.log('You chose '+ playerPick);
 		return playerPick;	
 	}
 }
 
-function game() {
-	
-	{let numberOfPlayerWins = 0;
-	let numberOfComputerWins = 0;
-	let score;
+function playGame() {
+
+{let numberOfPlayerWins = 0;
+let numberOfComputerWins = 0;
+let score;
 	
 	//function to play one round and declare winner of round
-let playRound = function(playerSelection, computerSelection) {
-	//to avoid "already declared" error in console, wrap in brackets to change scope
-	{let playerSelection = playerChoice();
-	let computerSelection = computerPlay();
+	let playRound = function(playerSelection, computerSelection) {
+		//to avoid "already declared" error in console, wrap in brackets to change scope
+		{let playerSelection = getPlayerChoice();
+		let computerSelection = getComputerPlay();
 
-	if ((playerSelection === 'rock' && computerSelection === 'scissors')
-|| (playerSelection === 'paper' && computerSelection === 'rock') 
-|| (playerSelection === 'scissors' && computerSelection === 'paper')) {
-	console.log('You win this round! ' + playerSelection + ' beats ' + computerSelection);
+		if ((playerSelection === 'rock' && computerSelection === 'scissors')
+			|| (playerSelection === 'paper' && computerSelection === 'rock') 
+			|| (playerSelection === 'scissors' && computerSelection === 'paper')) {
+			console.log('You win this round! ' + playerSelection + ' beats ' + 
+				computerSelection);
 
-	return score = 'The score is: \nYou - ' + (++numberOfPlayerWins) + ', Computer - ' + numberOfComputerWins;
-	}
+			return score = 'The score is: \nYou - ' + (++numberOfPlayerWins) +
+				', Computer - ' + numberOfComputerWins;
+		}
 
-	else if (playerSelection === computerSelection) {
-	console.log("It's a tie. No winner this round.");
-	return score = 'The score is: \nYou - ' + numberOfPlayerWins + ', Computer - ' + numberOfComputerWins;
-	}
-	else {
-	console.log('Dang. ' + computerSelection + ' beats ' + playerSelection + '. The computer wins this round.')
-	return score = 'The score is: \nYou - ' + numberOfPlayerWins + ', Computer - ' + (++numberOfComputerWins);
+		else if (playerSelection === computerSelection) {
+				console.log("It's a tie. No winner this round.");
+			return score = 'The score is: \nYou - ' + numberOfPlayerWins +
+				', Computer - ' + numberOfComputerWins;
+		}
+		else {
+				console.log('Dang. ' + computerSelection + ' beats ' + playerSelection +
+				'. The computer wins this round.')
+			return score = 'The score is: \nYou - ' + numberOfPlayerWins +
+				', Computer - ' + (++numberOfComputerWins);
 	}
 }};
 
@@ -55,12 +63,15 @@ for (let roundNumber = 1; roundNumber < 6; roundNumber++) {
 	console.log('ROUND '+ roundNumber);
 	console.log(playRound());
 }
-
-if (numberOfPlayerWins > numberOfComputerWins) {console.log('GAME OVER: You won the game!! Refresh page to play again.')}
-else if (numberOfPlayerWins < numberOfComputerWins) {console.log('GAME OVER: You did not win the game :( Refresh page to play again.')}
+if (numberOfPlayerWins > numberOfComputerWins) {
+		console.log('GAME OVER: You won the game!! Refresh page to play again.')
+}
+else if (numberOfPlayerWins < numberOfComputerWins) {
+		console.log('GAME OVER: You did not win the game :( Refresh page to play again.')
+}
 else if (numberOfComputerWins == numberOfPlayerWins) {
 	console.log('GAME OVER: You tied with the computer! Refresh page to play again.')
 }
 }}
 
-game();
+playGame();
